92c83e6a996576e439b83a94cc85f3b42bdc8d4a
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2024-10-14 15:39:15 +0530

    Time: 0 ms (100%), Space: 44.8 MB (28.25%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] ans = new int[2*n];
+        int[] ans = new int[nums.length];
         for(int i = 0; i < n; i++){
             ans[2*i] = nums[i];
             ans[2*i + 1] = nums[i +n];
