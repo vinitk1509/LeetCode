a78be3890539fc9de8682ad0af3ec782ba546ad0
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2024-10-27 22:45:25 +0530

    Time: 1 ms (82.24%), Space: 44 MB (58.97%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -3,7 +3,7 @@ class Solution {
         for (int i = 0 ; i < nums.length; i++){
             if (nums[i] == original){
                 original = original * 2 ;
-            i = -1;
+                i = -1;
             }
         }
         return original;
