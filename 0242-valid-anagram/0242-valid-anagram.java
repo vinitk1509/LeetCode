76ef03bb3a55d94351da4cfbe0acef8e81e3311d
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2024-11-25 12:55:08 +0530

    Time: 4 ms (76.28%), Space: 44.2 MB (57.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,29 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
        
-    if (s.length() != t.length()) {
-            return false;
-        }
-        List<Character> letter_s = new ArrayList<>();
-        List<Character> letter_t = new ArrayList<>();
+    // if (s.length() != t.length()) {
+    //         return false;
+    //     }
+    //     List<Character> letter_s = new ArrayList<>();
+    //     List<Character> letter_t = new ArrayList<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            letter_s.add(s.charAt(i));
-            letter_t.add(t.charAt(i));
-        }
+    //     for (int i = 0; i < s.length(); i++) {
+    //         letter_s.add(s.charAt(i));
+    //         letter_t.add(t.charAt(i));
+    //     }
 
-        Collections.sort(letter_s);
-        Collections.sort(letter_t);
+    //     Collections.sort(letter_s);
+    //     Collections.sort(letter_t);
 
-        return letter_s.equals(letter_t);
+    //     return letter_s.equals(letter_t);                   34 ms
+   
+        char [] letter_s=s.toCharArray();
+        char [] letter_t=t.toCharArray();
+        Arrays.sort(letter_s);
+        Arrays.sort(letter_t);
+        return Arrays.equals(letter_s, letter_t);   
     }
+
 
     
 }
