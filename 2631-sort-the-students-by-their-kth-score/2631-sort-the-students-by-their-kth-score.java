4d943b4a1f7f8548bd1ef4cf9837a61cc1f65625
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2024-12-10 09:24:30 +0530

    Time: 12 ms (5.45%), Space: 52.7 MB (8.55%) - LeetHub

--- 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.java
@@ -1,11 +1,19 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-    //     for(int i = 0 ; i < score.length; i++){
-    //         for(int j = 0 ; j < score[i].length; j++){
-    //             score[i][k] 
-    //         }
-    //     }
-    Arrays.sort(score, (a, b) -> Integer.compare(b[k], a[k])); // Descending order for kth exam scores
+        int m = score.length;
+        int n = score[0].length;
+        for(int i = 0 ; i < m-1; i++){
+            for(int j = 0 ; j < m-i-1; j++){
+                if(score[j][k] < score[j+1][k]){
+                    for(int col = 0; col < n; col++){
+                        int temp = score[j][col];
+                        score[j][col] = score[j+1][col];
+                        score[j+1][col] = temp;
+                    }
+                } 
+            }
+        }
+    // Arrays.sort(score, (a, b) -> Integer.compare(b[k], a[k])); // Descending order for kth exam scores
         
         return score;
     }
