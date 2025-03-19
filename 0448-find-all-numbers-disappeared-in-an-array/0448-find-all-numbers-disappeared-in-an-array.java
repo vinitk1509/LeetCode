a4cbeba69b769fce2ff0af28b814ff69c44d856c
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2025-03-19 07:43:41 +0530

    Time: 7 ms (51.42%), Space: 56.4 MB (81.24%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,32 +1,65 @@
+
 import java.util.*;
+
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-
-  int i = 0;
-        while (i < nums.length){
-            int correct = nums[i] - 1;
-            if (nums[i] != nums[correct]){
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1; // Correct index of nums[i]
+            
+            // Ensure within valid range and not already at correct position
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
                 swap(nums, i, correct);
             } else {
                 i++;
             }
         }
-        List <Integer> ans = new ArrayList<>();
-        for(int index = 0; index < nums.length; index++){
-            if(nums[index] != index+1){
-                ans. add(index + 1);
+
+        // Collect missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(index + 1);
             }
         }
         return ans;
     }
-    
-    static void swap (int[] nums, int first, int second){
+
+    private void swap(int[] nums, int first, int second) {
         int temp = nums[first];
         nums[first] = nums[second];
         nums[second] = temp;
     }
-
 }
+
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+
+//   int i = 0;
+//         while (i < nums.length){
+//             int correct = nums[i] - 1;
+//             if (nums[i] != nums[correct]){
+//                 swap(nums, i, correct);
+//             } else {
+//                 i++;
+//             }
+//         }
+//         List <Integer> ans = new ArrayList<>();
+//         for(int index = 0; index < nums.length; index++){
+//             if(nums[index] != index+1){
+//                 ans. add(index + 1);
+//             }
+//         }
+//         return ans;
+//     }
+    
+//     static void swap (int[] nums, int first, int second){
+//         int temp = nums[first];
+//         nums[first] = nums[second];
+//         nums[second] = temp;
+//     }
+
+// }
 // ---------------------------completely wrong------------------------------------ 
  
 //        Arrays.sort(nums);
