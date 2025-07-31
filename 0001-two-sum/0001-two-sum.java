03824ee0048b0ac8d255179212253656795517cc
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2025-07-31 23:46:03 +0530

    Time: 45 ms (30.73%), Space: 45.5 MB (5.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,10 +3,10 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+                    return new int[]{i, j};
                 }
             }
         }
-        throw new IllegalArgumentException("No solution found");
+        return new int[0]; 
     }
 }
